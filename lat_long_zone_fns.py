26d54d4959cdadc4bf09a1a5937ddacb38f07d8a
Nick Grifka <ngrifka@umich.edu>
2015-11-02 16:09:02 -0500

    Created "500 tracks in the US" location datasets

--- lat_long_zone_fns.py
+++ lat_long_zone_fns.py
@@ -1,5 +1,17 @@
 ZONE_LENGTH_IN_DEGREES = 10
 
+
+# Data set filters
+def isWithinUnitedStates(latitude, longitude):
+   """Returns true if the latitude longitude pair is roughly within the
+      boundaries of the United States. Returns false otherwise"""
+
+   return (25 < latitude and latitude < 50) and (-127 < longitude and longitude < -65)
+
+def all(latitude, longitue):
+   return True
+
+
 def LatLongToZoneNum(latitude, longitude):
    """Returns a zone number between 0 and 647, based on the given lat/long values.
       Zones are 10 degrees latitude by 10 degrees longitude chunks of area on the globe.
@@ -40,3 +52,38 @@ def ZoneNumToLatLongBoundary(zone_num):
    long_info = "Longitude range: " + str(min_long) + " to " + str(min_long + ZONE_LENGTH_IN_DEGREES)
    
    return lat_info, long_info;
+
+def unitedStatesLatLongToZoneNum(latitude, longitude):
+   """Returns a zone number that the input location coordinate belongs to.
+      Assumes the input location coordinate is within the United States.
+      This function uses 4 zones in total to split up the US into quadrants"""
+
+   # North East
+   if (latitude >= 38 and longitude >= -95):
+      return 1
+
+   # North West
+   if (latitude >= 38 and longitude < -95):
+      return 2
+
+   # South East
+   if (latitude < 38 and longitude >= -95):
+      return 3
+
+   # South West
+   if (latitude < 38 and longitude < -95):
+      return 4
+
+def unitedStatesZoneNumToRegion(zone_num):
+   """Returns a string that describes the given zone number.
+      Assumes zone_num is a value produced by the function unitedStatesLatLongToZoneNum()."""
+
+   if (zone_num == 1):
+      return 'North East'
+   if (zone_num == 2):
+      return 'North West'
+   if (zone_num == 3):
+      return 'South East'
+   if (zone_num == 4):
+      return 'South West'
+
--- track_location_script.py
+++ track_location_script.py
@@ -2,9 +2,9 @@ import sys
 import lat_long_zone_fns as llz
 
 
-def generateTrackLocationFile(inputFileName, outputFileName):
+def generateTrackLocationFile(input_data_file_name, output_data_file_name, output_label_file_name, filter_func, label_func, num_data_points_needed):
    # Read in the necessary files
-   track_fd = open(inputFileName, 'r')
+   track_fd = open(input_data_file_name, 'r')
    matches_fd = open('mxm/mxm_779k_matches.txt', 'r')
    artist_fd = open('mxm/unique_artists.txt', 'r')
    location_fd = open('mxm/artist_location.txt', 'r')
@@ -23,7 +23,10 @@ def generateTrackLocationFile(inputFileName, outputFileName):
       latitude = float(line[sep1_idx+5:sep2_idx])
       longitude = float(line[sep2_idx+5:sep3_idx])
 
-      artist_locations[artist_id] = llz.LatLongToZoneNum(latitude, longitude)
+      # Only keep track of this location info based on passed in functions
+      if (filter_func(latitude, longitude)):
+         artist_locations[artist_id] = label_func(latitude, longitude)
+
 
    # Create artist name -> artist id map w/ unique artist file
    duplicate_artist_names = {}
@@ -77,14 +80,18 @@ def generateTrackLocationFile(inputFileName, outputFileName):
    total_tracks = 0
    track_locations_list = []
 
+   output_data_fd = open(output_data_file_name, 'w')
+
    # Go through each track in the given track file
    for line in track_fd.readlines():
       # Skip the comments in the beginning of the file
       if (line[0] == '#'):
+         output_data_fd.write(line)
          continue
 
       # Skip the line with the 5000 words
       if (line[0] == '%'):
+         output_data_fd.write(line)
          continue
 
       track_id = line[:line.index(',')]
@@ -95,6 +102,8 @@ def generateTrackLocationFile(inputFileName, outputFileName):
          if (track_artists[track_id] in artist_ids):
             if (artist_ids[track_artists[track_id]] in artist_locations):
                track_locations_list.append((track_id, artist_locations[artist_ids[track_artists[track_id]]]))
+               output_data_fd.write(line)
+               num_data_points_needed -= 1
             else:
                locations_not_found += 1
          else:
@@ -102,12 +111,17 @@ def generateTrackLocationFile(inputFileName, outputFileName):
       else:
          matches_not_found += 1
 
+      # Stop if we got all the data points we need
+      if (num_data_points_needed == 0):
+         break
+
    # At the end, write the results to disk
-   clean_result_file = open(outputFileName, 'w')
+   clean_result_file = open(output_label_file_name, 'w')
    for entry in track_locations_list:
       clean_result_file.write(entry[0] + ' ' + str(entry[1]) + '\n')
 
    clean_result_file.close();
+   output_data_fd.close();
 
    # Display some statistics
    total_not_found = matches_not_found + artists_not_found + locations_not_found
@@ -123,11 +137,22 @@ def generateTrackLocationFile(inputFileName, outputFileName):
    print 'Number of duplicated artist names: ' + str(len(duplicate_artist_names))
 
 def main(argv):
-   if (len(argv) != 3):
-      print 'Incorrect usage: >> python song_location_matcher.py <input filename> <output filename>'
+   if (len(argv) < 5):
+      print 'Incorrect usage: >> python song_location_matcher.py <[all|US]> <dataset input filename> <dataset output filename> <label output filename> <num data points (optional)'
       return
 
-   generateTrackLocationFile(argv[1], argv[2])
+   num_data_points = sys.maxint
+   # The user specified how many data points they want
+   if (len(argv) == 6 and int(argv[5]) > 0):
+      num_data_points = int(argv[5])
+
+   # Supply different filter arguments based on user input
+   if (argv[1] == 'all'):
+      generateTrackLocationFile(argv[2], argv[3], argv[4], llz.all, llz.LatLongToZoneNum, num_data_points)
+   elif (argv[1] == 'US'):
+      generateTrackLocationFile(argv[2], argv[3], argv[4], llz.isWithinUnitedStates, llz.unitedStatesLatLongToZoneNum, num_data_points)
+   else:
+      print 'Invalid filter given: ' + str(argv[1])
 
 
 if __name__ == "__main__":
